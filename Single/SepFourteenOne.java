1c8bb68f670e17cd2aabf8d3eed4da8afe67d5d5
Daniel Ye <danielyeqt@gmail.com>
2021-09-14 12:53:50 -0400

    September 14 - 1

---
+++ Single/SepFourteenOne.java
@@ -0,0 +1,18 @@
+/* Daniel Ye
+* September 14, 2021
+* Ms. Basaraba
+* This program prints a string and acts as the testing class for the in class lesson */
+
+import java.awt.*; // * = import all classes from the package
+import hsa.Console;
+
+public class SepFourteenOne // progam heading & gives the program its name public can be accessed by other programs
+{
+    static Console c; // 
+    
+    public static void main (String[] args) // main method is the starting point
+    {
+	c = new Console (); // creates a new object (called c) of type Console
+	
+    }
+}
