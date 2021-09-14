78d70ef3f418254fd4785a83cf14bf1c97e486c1
Daniel Ye <danielyeqt@gmail.com>
2021-09-14 13:01:25 -0400

    Gitignore & stuff

--- Single/SepFourteenOne.java
+++ Single/SepFourteenOne.java
@@ -8,11 +8,12 @@ import hsa.Console;
 
 public class SepFourteenOne // progam heading & gives the program its name public can be accessed by other programs
 {
-    static Console c; // 
+    static Console c;
     
     public static void main (String[] args) // main method is the starting point
-    {
+    { // brackets indicate the beginning and end of a block of code
 	c = new Console (); // creates a new object (called c) of type Console
+	c.println("hello people this is a print statement wow boop aksdfjwre");
 	
-    }
-}
+    } // main method
+} // SepFourteenOne class
